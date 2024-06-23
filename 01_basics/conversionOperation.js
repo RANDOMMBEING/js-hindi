0bf1d6cdfa86aedd8a76e3c7563898d72232f432
DAKSH SINGHAL <165793932+RANDOMMBEING@users.noreply.github.com>
2024-06-23 12:10:23 +0000

    conversions

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -1,11 +1,11 @@
 let score = null
 
-console.log(typeof score);
+//console.log(typeof score);
 
 let valueInNumber = Number(score) //score is now converted into number
-console.log(typeof (valueInNumber));
+//console.log(typeof (valueInNumber));
 
-console.log(valueInNumber) // NaN stated for not a number
+//console.log(valueInNumber) // NaN stated for not a number
 
 // "33" => 33
 // "33abc" =>NaN although the typ of 33abc will be shown number only
@@ -15,9 +15,36 @@ console.log(valueInNumber) // NaN stated for not a number
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+//console.log(booleanIsLoggedIn)
 
 // 1=> true
 // => false
 // "" => false
 // "daksh" => true
+
+
+let value = 3
+let negValue = -value
+//console.log(negValue)
+// 2+2
+// 2-2
+// 2**2
+// 2*2
+// 2/3
+// 2%3
+
+let str1 = "hello"
+let str2 = " daksh"
+
+let str3 = str1 + str2
+//console.log(str3)
+
+//console.log("1" + 2 +2); gives 122 emca
+//console.log(1 + 2 + "2"); gives 32 emca
+
+
+//console.log(+true) gives 1
+//console.log("")  gives nothing
+//console.log(+"") gives 0
+
+// prefic and postfix increment operator mdn docks
