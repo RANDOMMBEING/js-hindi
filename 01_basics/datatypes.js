e33ad7218863a6c588066a50d09d0d6158ded245
DAKSH SINGHAL <165793932+RANDOMMBEING@users.noreply.github.com>
2024-06-23 09:31:27 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as newer version
+
+//alert( 3 + 3) // we are using node.js not browser
+
+//code readability should be high
+
+let name = "daksh" //string
+let age = 18 //number
+let isLoggedIn = false //boolean
+let state;
+
+
+// number => 2 to power 53
+//bigint it is used when your number is very big
+// string => "" or '' but prefer double quotes
+// boolean => true/false
+// null => standalone value
+// undefined => value is not defined
+// symbol=> unique
+
+// object
+
+console.log(typeof "daksh")
