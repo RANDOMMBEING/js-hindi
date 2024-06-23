9e3f709ddc9f3009af6b6a1e27295c7e775c9b71
DAKSH SINGHAL <165793932+RANDOMMBEING@users.noreply.github.com>
2024-06-23 12:26:03 +0000

    comparison

---
+++ 01_basics/comparison.js
@@ -0,0 +1,26 @@
+
+// console.log(2>1)
+// console.log( 2<1)
+
+// problem wahan aati hai jabh hum same datatype ko compare nhi karte
+
+//console.log("2" > 1);
+//console.log("02" > 1);
+
+/*console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+
+never use these type of comparisons 
+#clean code */
+
+//  == and ( >  <  >=  <= )work differently
+// comparisons conver null to a number treating it as 0
+// that's why null>=0 is true and null>0 is false
+
+// === strict check not only checks the value but also the datatype
+
